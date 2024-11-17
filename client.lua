b12363147afd93eefbe82713702d04e4d2bdfb48
Sina <santiano38@googlemail.com>
2024-11-17 17:53:02 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -17,16 +17,12 @@ RegisterNetEvent('hathair:toggleHair', function()
         -- If male, set hair to 0 (bald)
         if gender == 0 then
             SetPedComponentVariation(ped, 2, 0, 0, 2) -- Set hair drawable to 0 (bald)
-            QBCore.Functions.Notify("You put on a hat and your hair disappeared!", "success")
-        else
-            QBCore.Functions.Notify("This command is only for male characters.", "error")
         end
     else
         -- Restore the saved hairstyle
         local savedHair = savedHairStyles[GetPlayerServerId(PlayerId())]
         SetPedComponentVariation(ped, 2, savedHair.drawable, savedHair.texture, 2) -- Restore the saved hair drawable and texture
         savedHairStyles[GetPlayerServerId(PlayerId())] = nil
-        QBCore.Functions.Notify("You took off the hat and your hair is back!", "success")
     end
 end)
 
